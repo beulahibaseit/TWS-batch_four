9659af4062b062ff913cacfd5daf1e8ad9b42b0e
beulahibaseit <gentleasjesus@gmail.com>
2023-08-18 01:00:04 +0530

    changes made to python_intro file

---
+++ python_intro.py
@@ -0,0 +1,18 @@
+Python is a famous programming language.
+Python is discovered by Guido Van Rossum in 1991.
+Python is used for:
+1. Web Development
+2. Software Development
+3. Mathematics
+4. System Scripting
+
+Uses:
+Python is an easy and understandable language with simple english.
+Python is used in web develpment, big data, and perform complex mathematics.
+Python can connect to databse systems. It can read n modify files.
+Python can be used alongside software to create workflows.
+Python can be used for rapid-prototyping and production-ready software development.
+
+Python can be written in fewer lines than compared to other Programming languages.
+Python can be used on various platforms like linux, Mac, windows, Rasberry, pi, etc.
+Python follows indentation and white spaces, whereas other PL used culry braces.
